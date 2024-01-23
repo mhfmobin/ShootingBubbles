b1cbfe2df7c2a5b16e31eb0f0006bdb7edbdba84
MH Farahmand ツ <mhfmobin@gmail.com>
2024-01-23 11:47:45 +0330

    Setup base of project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+    Project: Bouncing Balls
+    Authors: MH Farahmand ツ, AS Anghaei
+    Date: January, 2024
+    Description: Bouncing Balls game written in C++ - EE Basic Programming Final Project
+
+    License: MIT License
+*/
+
+#include "packages.h"
+#include "front-end.h"
+#include "back-end.h"
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
---
+++ packages.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfx.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
