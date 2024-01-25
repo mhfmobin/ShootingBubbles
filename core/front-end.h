f49cbe4b1846a2771b461806fbae963f56b3b84b
AS-Anghaei <76442491+AS-Anghaei@users.noreply.github.com>
2024-01-25 17:22:41 +0330

    Update front-end.h

--- core/front-end.h
+++ core/front-end.h
@@ -1,10 +1,97 @@
-#ifndef BOUNCINGBALLS_FRONT-END_H
-#define BOUNCINGBALLS_FRONT-END_H
+#ifndef BOUNCINGBALLS_FRONT_END_H
+#define BOUNCINGBALLS_FRONT_END_H
+
+void DrawMenu(SDL_Renderer* renderer);
+void Draw(SDL_Renderer* renderer, SDL_Texture* m_img, SDL_Rect img_rect, int x, int y, int w, int h);
+void Modes(SDL_Renderer* renderer);
+void Levels(SDL_Renderer* renderer);
+void Setting(SDL_Renderer* renderer);
+void PlayMusic(Mix_Chunk* music,int volume,int repeat,bool can_play);
 
 
 
+//_______________________functions______________________________
+
+void DrawMenu(SDL_Renderer* renderer){
+    Draw(renderer,menu_img,menu_rect,0,0,WIDTH,HEIGHT);
+}
+
+void Draw(SDL_Renderer* renderer, SDL_Texture* m_img, SDL_Rect img_rect, int x, int y, int w, int h)
+{
+    img_rect.x = x;
+    img_rect.y = y;
+    img_rect.w = w;
+    img_rect.h = h;
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, m_img, NULL, &img_rect);
+    SDL_RenderPresent(renderer);
+}
+
+void Modes(SDL_Renderer* renderer){
+    SDL_PollEvent(e);
+    SDL_ShowCursor(SDL_ENABLE);
+    mouse_x = e->button.x;
+    mouse_y = e->button.y;
+    Draw(renderer,modes_img,modes_rect,0,0,WIDTH,HEIGHT);
+    if( e -> type == SDL_QUIT){
+        modes=false;
+    }
+    if((e->key.keysym.sym == SDLK_ESCAPE && e ->type == SDL_KEYDOWN) )
+    {
+        e ->type = 0 ;
+        modes = false;
+    }
+    if((mouse_x>160)&&(mouse_x<313)&&(mouse_y>180)&&(mouse_y<223) && (e->button.button == SDL_BUTTON_LEFT && e->type == SDL_MOUSEBUTTONDOWN)){
+        levels=true;
+        modes=false;
+        PlayMusic(btn_sd,25,0,btn_sd_c);
+    }
+}
+
+void Levels(SDL_Renderer* renderer){
+    SDL_PollEvent(e);
+    SDL_ShowCursor(SDL_ENABLE);
+    mouse_x = e->button.x;
+    mouse_y = e->button.y;
+    Draw(renderer,levels_img,levels_rect,0,0,WIDTH,HEIGHT);
+    if( e -> type == SDL_QUIT){
+        levels=false;
+    }
+    if((e->key.keysym.sym == SDLK_ESCAPE && e ->type == SDL_KEYDOWN) )
+    {
+        e ->type = 0 ;
+        levels = false;
+    }
+    if((mouse_x>24)&&(mouse_x<205)&&(mouse_y>252)&&(mouse_y<297) && (e->button.button == SDL_BUTTON_LEFT && e->type == SDL_MOUSEBUTTONDOWN)){
+        PlayMusic(btn_sd,25/2,0,btn_sd_c);
+    }
+}
+
+void Setting(SDL_Renderer* renderer){
+    SDL_PollEvent(e);
+    SDL_ShowCursor(SDL_ENABLE);
+    mouse_x = e->button.x;
+    mouse_y = e->button.y;
+    Draw(renderer,setting_img,setting_rect,0,0,WIDTH,HEIGHT);
+    if( e -> type == SDL_QUIT){
+        setting=false;
+    }
+    if((e->key.keysym.sym == SDLK_ESCAPE && e ->type == SDL_KEYDOWN) )
+    {
+        e ->type = 0 ;
+        setting = false;
+    }
+    if((mouse_x>24)&&(mouse_x<205)&&(mouse_y>252)&&(mouse_y<297) && (e->button.button == SDL_BUTTON_LEFT && e->type == SDL_MOUSEBUTTONDOWN)){
+        PlayMusic(btn_sd,25,0,btn_sd_c);
+    }
+}
+
+void PlayMusic(Mix_Chunk* music,int volume,int repeat,bool can_play){
+    if(can_play){
+        Mix_VolumeChunk(music, volume);
+        Mix_PlayChannel(-1, music, repeat);
+    }
+}
 
 
-
-
-#endif //BOUNCINGBALLS_FRONT-END_H
+#endif //BOUNCINGBALLS_FRONT_END_H
